89f32492c2982afdb3dca5dff389e8640ea763c7
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-05-12 16:50:51 +0200

    init gin

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello, world!")
+	router := gin.Default()
+
+	router.GET("/api/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run("0.0.0.0:8080")
 }
